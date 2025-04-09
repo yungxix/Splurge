a36617be4b7a992ed5f28eec1060a6d063206205
Ifelere Oyetola <ifelere@gmail.com>
2025-04-09 20:59:33 +0100

    Update AppServiceProvider.php

--- splurge-app/app/Providers/AppServiceProvider.php
+++ splurge-app/app/Providers/AppServiceProvider.php
@@ -25,6 +25,12 @@ class AppServiceProvider extends ServiceProvider
             return $app["splurge.access"];
         });
 
+        if (env('HOSTED')) {
+            $this->app->bind('path.public', function () {
+                return getcwd();
+            });
+        }        
+
         
     }
 
@@ -35,11 +41,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('HOSTED')) {
-            $this->app->bind('path.public', function () {
-                return getcwd();
-            });
-        }        
+       
         if (config('logging.log_queries')) {
             DB::listen(function (QueryExecuted $query) {
                 Log::debug("[Query] {$query->sql}", [
